b90b7cfc2559da596308a149b78624d95cd4a207
Priyanka Devi <priyanka.devi@ui.ac.id>
2023-06-25 11:14:18 +0700

    Add url in task js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://main--superlative-fox-8d75a8.netlify.app/" // TODO: replace this
 const studentName = "Priyanka Devi" // TODO: replace this
 const studentId = "FE4529989" // TODO: replace this
 
