03de0fa3eaf481705293e6a30220e3543b69a5e2
Priyanka Devi <priyanka.devi@ui.ac.id>
2023-06-25 11:11:12 +0700

    Add rest of the file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Priyanka Devi" // TODO: replace this
+const studentId = "FE4529989" // TODO: replace this
+
+export { url, studentName, studentId }
